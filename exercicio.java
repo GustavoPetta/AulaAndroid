e0b8b6d3d7227a9cfe55554d0b29739b3f5ef7df
Gustavo Petta <gustavopetta29@hotmail.com>
2020-03-23 01:32:25 -0300

    Aula Java

--- exercicio.java
+++ exercicio.java
@@ -1,5 +1,5 @@
 AlertDialog.Builder  alert = new AlertDialog.Builder(this);
 alert.setTitle("Alert")
-alert.setMessage("Preencha o campo vazio");
+alert.setMessage("Preencha este campo");
 alert.setNeutralButton("Ok",null);
 alert.show();
